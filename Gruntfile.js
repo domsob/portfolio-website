dbfad0410d4a367f5bf8f7f49856b1397463b5df
Dominik Sobania <dominik.sobania@codeforcemedia.com>
2016-02-17 11:19:28 +0100

    Grunt process optimized.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,33 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean : {
+            target : {
+                src : [ "build/*"]
+            }
+        },
         uglify: {
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
+            options: {
+              mangle: {
+                  except: [
+                    'js/bootstrap.min.js',
+                    'js/jquery.easing.min.js'
+                  ]
+              }
+            },
+            library: {
+                files: {
+                    'build/js/main.min.js': [
+                      'js/jquery.js',
+                      'js/bootstrap.min.js',
+                      'js/jquery.easing.min.js',
+                      'js/classie.js',
+                      'js/cbpAnimatedHeader.js',
+                      'js/jqBootstrapValidation.js',
+                      'js/contact_me.js',
+                      'js/freelancer.js'
+                    ]
+                }
             }
         },
         less: {
@@ -17,32 +40,57 @@ module.exports = function(grunt) {
                 files: {
                     "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
                 }
-            },
-            minified: {
-                options: {
-                    paths: ["css"],
-                    cleancss: true
-                },
-                files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
-                }
             }
         },
-        banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
-            ' */\n',
-        usebanner: {
-            dist: {
-                options: {
-                    position: 'top',
-                    banner: '<%= banner %>'
-                },
-                files: {
-                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
-                }
+        cssmin: {
+          options: {
+            shorthandCompacting: false,
+            roundingPrecision: -1,
+            keepSpecialComments: 0
+          },
+          target: {
+            files: {
+              'build/css/style.min.css': [
+                'css/bootstrap.css',
+                'css/freelancer.css',
+                'font-awesome/css/font-awesome.min.css'
+              ]
             }
+          }
+        },
+        htmlclean: {
+            root: {
+              expand: true,
+              cwd: '',
+              src: '*.html',
+              dest: 'build/'
+            }
+        },
+        copy: {
+          options: {
+              processContentExclude: ['**/*.{png,gif,jpg,ico,svg,ttf,eot,woff}']
+          },
+          target: {
+            files: [
+                {expand: true, src: ['index.html'], dest: 'build/'},
+                {expand: true, src: ['img/portfolio/*'], dest: 'build/'},
+                {expand: true, src: ['img/*.png'], dest: 'build/'},
+                {expand: true, src: ['img/*.jpg'], dest: 'build/'},
+                {expand: true, src: ['img/*.jpeg'], dest: 'build/'},
+                {expand: true, src: ['img/*.gif'], dest: 'build/'},
+                {expand: true, src: ['mail/*'], dest: 'build/'},
+                {
+                  expand: false,
+                  src: ['font-awesome/fonts/fontawesome-webfont.woff'],
+                  dest: 'build/fonts/fontawesome-webfont.woff'
+                },
+                {
+                  expand: false,
+                  src: ['font-awesome/fonts/fontawesome-webfont.ttf'],
+                  dest: 'build/fonts/fontawesome-webfont.ttf'
+                }
+            ],
+          }
         },
         watch: {
             scripts: {
@@ -63,12 +111,15 @@ module.exports = function(grunt) {
     });
 
     // Load the plugins.
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-htmlclean');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
+    grunt.registerTask('default', ['clean', 'uglify', 'less', 'cssmin', 'copy', 'htmlclean']);
 
 };
